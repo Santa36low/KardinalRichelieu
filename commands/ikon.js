177f12537f13fab37e6f51e6fb51c98c4c578c33
Santa36low <58489847+Santa36low@users.noreply.github.com>
2019-12-06 19:19:16 +0100

    Add files via upload

---
+++ commands/ikon.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    message.channel.send('Ikonky najdeš na ☯ https://www.prosettings.com/emoji-list/#av-symbol ☯');
+
+};
+
+module.exports.help = {
+    name: 'ikon',
+    aliases: []
+};
